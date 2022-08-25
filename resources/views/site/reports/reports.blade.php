d37a13365e7e6313d6102992d49c36b296a32598
Kartikay Gandhi <kartikaygandhi123@gmail.com>
2022-08-25 22:45:05 +0530

    working filters with styled css

--- resources/views/site/reports/reports.blade.php
+++ resources/views/site/reports/reports.blade.php
@@ -4,6 +4,8 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <link href="/assets/vendor_components/select2/dist/css/select2.min.css" rel="stylesheet" />
 
+
+
     <div class="box box-solid box-primary">
         <div class="box-header with-border">
             <h4 class="box-title">Reports</h4>
@@ -12,95 +14,110 @@
         <!-- /.box-header -->
         <div class="box-body">
 
+            <div class="pull-right">
+                <button id="filter_button" class="btn btn-sm btn-primary "><i class="fa fa-filter">Filters</i> </button>
+                <br>
+                <br>
+
+
+            </div>
+
             {{-- ........................................................................................................................................................... --}}
+            <div id="hidden_filters" class="box box-solid box-light" style="display:none">
+                <form action="/filterresult" method="POST">
+                    @csrf
+                    <div class="row filter-row">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="form-group form-focus">
+                                <label class="focus-label ">Customer Name</label>
+                                <select class="select2 form-control floating" id="" name='name[]' multiple
+                                    style="width: 100%">
 
-            <form action="/filterresult" method="POST">
-                @csrf
-                <div class="row filter-row">
-                    <div class="col-sm-6 col-md-2 col-lg-2">
-                        <div class="form-group form-focus">
-                            <label class="focus-label">Customer Name</label>
-                            <select class="select2 form-control floating" id="" name='name[]' multiple>
-
-                                <option value="">-- Select --</option>
-                                @foreach ($leads as $name)
-                                    <option value="{{ $name->Customer_Name }}">{{ $name->Customer_Name }}</option>
-                                @endforeach
-                            </select>
+                                    {{-- <option value="">-- Select --</option> --}}
+                                    @foreach ($leads as $name)
+                                        <option value="{{ $name->Customer_Name }}">{{ $name->Customer_Name }}</option>
+                                    @endforeach
+                                </select>
 
 
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-2 col-lg-2">
-                        <div class="form-group form-focus select-focus">
-                            <label class="focus-label">Stage</label>
-                            <select class="select2 form-control floating" id="" name='stage[]' multiple>
-
-                                <option value="">-- Select --</option>
-                                @foreach ($stage as $stage)
-                                    <option value="{{ $stage->stage }}">{{ $stage->stage }}</option>
-                                @endforeach
-                            </select>
-
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-md-2 col-lg-2">
-                        <div class="form-group form-focus select-focus">
-                            <label class="focus-label">Lead Source</label>
-                            <select class="select2 form-control floating" id="" name="lead_source[]" multiple>
-                                <option value="">Select --</option>
-                                @foreach ($lead_source as $lead_source)
-                                    <option value="{{ $lead_source->lead_source }}">{{ $lead_source->lead_source }}</option>
-                                @endforeach
-                            </select>
-
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-2 col-lg-2">
-                        <div class="form-group form-focus select-focus">
-                            <label class="focus-label">User</label>
-                            <select class="select2 form-control floating" id="" name="user[]" multiple>
-                                <option value="">Select --</option>
-                                @foreach ($user as $user)
-                                    <option value="{{ $user->id }}">{{ $user->name }}
-                                    </option>
-                                @endforeach
-                            </select>
-
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-md-3 col-lg-3">
-
-                        <div class="form-group form-focus select-focus">
-                            <label class="focus-label">DateRange</label>
-
-                            <div class="input-group">
-                                <div class="input-group-addon">
-                                    <i class="fa fa-calendar"></i>
-                                </div>
-
-                                <input name="daterange" type="text" class="form-control pull-right" id="reservation" />
                             </div>
                         </div>
-                        <!-- /.input group -->
-                    </div>
-                    <input id="from" type="hidden" name="from">
-                    <input id="to" type="hidden" name="to">
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="form-group form-focus select-focus">
+                                <label class="focus-label">Stage</label>
+                                <select class="select2 form-control floating" id="" name='stage[]' multiple
+                                    style="width: 100%">
 
-                    <div class="col-sm-6 col-md-1 col-lg-1">
-                        <div class="form-group form-focus select-focus">
-                            <label class="focus-label"><i class="fa fa-filter">Filter</i></label>
-                            <button type="submit" class="btn btn-sm btn-bitbucket w-fit-content">Filter</button>
+                                    {{-- <option value="">-- Select --</option> --}}
+                                    @foreach ($stage as $stage)
+                                        <option value="{{ $stage->stage }}">{{ $stage->stage }}</option>
+                                    @endforeach
+                                </select>
+
+                            </div>
                         </div>
+
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="form-group form-focus select-focus">
+                                <label class="focus-label">Lead Source</label>
+                                <select class="select2 form-control floating" id="" name="lead_source[]" multiple
+                                    style="width: 100%">
+                                    {{-- <option value="">Select --</option> --}}
+                                    @foreach ($lead_source as $lead_source)
+                                        <option value="{{ $lead_source->lead_source }}">{{ $lead_source->lead_source }}
+                                        </option>
+                                    @endforeach
+                                </select>
+
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-2 col-lg-2">
+                            <div class="form-group form-focus select-focus">
+                                <label class="focus-label">User</label>
+                                <select class="select2 form-control floating" id="" name="user[]" multiple
+                                    style="width: 100%;border-color:blue">
+                                    {{-- <option value="">Select --</option> --}}
+                                    @foreach ($user as $user)
+                                        <option value="{{ $user->id }}">{{ $user->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-3 col-lg-3">
+
+                            <div class="form-group form-focus select-focus">
+                                <label class="focus-label">DateRange</label>
+
+                                <div class="input-group">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </div>
+
+                                    <input name="daterange" type="text" class="form-control pull-right"
+                                        id="reservation" />
+                                </div>
+                            </div>
+                            <!-- /.input group -->
+                        </div>
+                        <input id="from" type="hidden" name="from">
+                        <input id="to" type="hidden" name="to">
+
+                        <div class="col-sm-6 col-md-1 col-lg-1">
+                            <div class="form-group form-focus select-focus">
+                                <label class="focus-label"><i class="fa fa-filter"></i></label>
+                                <button type="submit" class="btn btn-sm btn-success w-fit-content">Search</button>
+                            </div>
+                        </div>
+
+
+
                     </div>
+                </form>
 
-
-
-                </div>
-            </form>
-
+            </div>
 
 
 
@@ -172,8 +189,19 @@
 
     <script src="/assets/vendor_components/select2/dist/js/select2.min.js"></script>
 
+
     <script>
-        $('.select2').select2();
+        $('#filter_button').on('click', function() {
+
+            $('#hidden_filters').toggle();
+
+
+        })
+    </script>
+    <script>
+        $('.select2').select2({
+            width: 'resolve'
+        });
     </script>
     <script>
         $('input[name="daterange"]').daterangepicker();
